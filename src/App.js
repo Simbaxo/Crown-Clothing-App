922b232c5bac89bc207b981cee265409f65dfe03
Andres Mena <ballerlikemike23@gmail.com>
2019-07-08 02:00:45 -0400

    Routing Examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch only render the first match */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        {/* Quick Example of Router */}
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
