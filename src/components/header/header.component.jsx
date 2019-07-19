d18eddec0433b1978b1ff69de1bd949a288294c6
Andres Mena <ballerlikemike23@gmail.com>
2019-07-19 00:07:55 -0400

    Header Styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
 const Header = () => (
-  <div className=''>
+  <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
