c88d2b2903406d49a44d752e62646c03d9779992
Andres Mena <ballerlikemike23@gmail.com>
2019-07-18 23:37:41 -0400

    Minor styling update

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,15 +3,15 @@ import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="Collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
       {items
         // filter to show only 4 items
         .filter((item, idx) => idx < 4)
         // map to loop over each item
-        .map(({ id, ...itemProps }) => (
-          <CollectionItem key={id} {...itemProps} />
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
