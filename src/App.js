4c7469e7f162372e1405a4c7dec7c4f2210e7238
Andres Mena <ballerlikemike23@gmail.com>
2019-07-30 19:55:21 -0400

    Create Sign In Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
 import './App.css';
 
@@ -12,8 +13,8 @@ function App() {
       {/* Switch only render the first match */}
       <Switch>
         <Route exact path='/' component={HomePage} />
-        {/* Quick Example of Router */}
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
