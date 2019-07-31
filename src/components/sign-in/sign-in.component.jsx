d4fe4cb91093aeb2a7bf25da43dc8daa938deef0
Andres Mena <ballerlikemike23@gmail.com>
2019-07-30 20:19:31 -0400

    Event Handlers for Sign In Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -11,16 +11,42 @@ class SignIn extends Component {
     }
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    // reset the state
+    this.setState({ email: '', password: '' })
+  }
+
+  handleChange = event => {
+    // Pulls value and name from event.target
+    // event.target is the input
+    const { value, name } = event.target
+    // dynamically sets state to inputs from user
+    this.setState({ [name]: value })
+  }
+
   render() {
     return (
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form>
-          <input name='email' value={this.state.email} required />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name='email'
+            type='email'
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
           <label>Email</label>
-          <input name='password' value={this.state.password} required />
+          <input
+            name='password'
+            type='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
           <label>Password</label>
 
           <input type='submit' value='Submit Form' />
