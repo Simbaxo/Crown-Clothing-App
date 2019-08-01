e46f09e8d536afbebfcdc09fd92416e333fcd040
Andres Mena <ballerlikemike23@gmail.com>
2019-07-31 20:44:20 -0400

    Styles for Custom Button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './custom-button.styles.scss'
 
 const CustomButton = ({ children, ...otherProps }) => (
+  // Passing the children and other props from input components
   <button className='custom-button' {...otherProps}>
     {children}
   </button>
