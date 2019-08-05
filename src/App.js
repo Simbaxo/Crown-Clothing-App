88c6c26e64c5059f0bf4a6251a3ddfab3533853d
Andres Mena <ballerlikemike23@gmail.com>
2019-08-05 19:33:02 -0400

    Connecting Firebase auth user state to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { Switch, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
-import './App.css';
+import { auth } from './firebase/firebase.utils'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      {/* Switch only render the first match */}
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* Switch only render the first match */}
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
