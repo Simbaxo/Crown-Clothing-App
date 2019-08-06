46b91337c4d90b59f6b2576add537b56b8c35039
Andres Mena <ballerlikemike23@gmail.com>
2019-08-05 20:28:57 -0400

    Storing Users into Firebase Database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import './App.css'
 
 class App extends Component {
@@ -19,10 +19,8 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
     })
   }
 
